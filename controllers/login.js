39a0ead978faabb956e8ea7094048aade72b6bea
pashateuku <thariqteuku.soc@gmail.com>
2022-05-23 01:31:25 +0700

    add notification status to admin login page

--- controllers/login.js
+++ controllers/login.js
@@ -28,14 +28,14 @@ async function loginPost(req, res) {
     });
   
     if (!userData) { // dilanjutkan mengecek email, apabila email tidak ditemukan maka:
-      return res.render('login.ejs', {
+      return res.status(403).render('login.ejs', {
         failed : 1
       });
 
     }
 
     if (userData.password !== password) { // dilanjutkan mengecek password, apabila password pada email yg digunakan salah maka:
-      return res.render('login.ejs', {
+      return res.status(403).render('login.ejs', {
         failed : 2
       });
     }
@@ -49,7 +49,7 @@ async function loginPost(req, res) {
 // controller untuk login method GET untuk admin
 function loginGetAdmin(req, res) {
     res.status(200)
-    return res.render('login_admin.ejs');
+    return res.render('login_admin.ejs', { failed : 0 });
 }
 
 // controller untuk login method GET untuk admin (cek email dan pass)
@@ -61,10 +61,10 @@ function loginPostAdmin(req, res) {
     const findDataIndex = adminUser.findIndex((element) => element.email == inputEmail);
 
     if (findDataIndex == -1) { // apabila hasil findIndex adalah -1, maka data sesuai syarat tidak ditemukan
-        res.status(403).json({ message: 'Email is not registered' });
+        res.status(403).render('login_admin.ejs', { failed : 1 });
     } else { // apabila data ditemukan dan--
             if (adminUser[findDataIndex].password != inputPassword) { // password salah
-                res.status(403).json({ message: 'Wrong Password' });
+                res.status(403).render('login_admin.ejs', { failed : 2 });
             } else { // password sesuai
                 res.status(200).json({ message: 'Login successful' });
             };            
